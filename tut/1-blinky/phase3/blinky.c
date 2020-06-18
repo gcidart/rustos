008988a829b411bcc08e29c25ece9e0fde1647a5
gcidart <gcidart@gmail.com>
2020-06-18 13:42:41 +0000

    lab2 done

--- tut/1-blinky/phase3/blinky.c
+++ tut/1-blinky/phase3/blinky.c
@@ -15,6 +15,14 @@ static void spin_sleep_ms(unsigned int ms) {
 }
 
 int kmain(void) {
-  // FIXME: STEP 1: Set GPIO Pin 16 as output.
+  //  STEP 1: Set GPIO Pin 16 as output.
+  *GPIO_FSEL1 |= (1<<18);   // GPIO_FSEL1[20:18] = Function Select 16
   // FIXME: STEP 2: Continuously set and clear GPIO 16.
+  while(1)
+  {
+	  *GPIO_SET0 |= (1<<16); //GPIO_SET0[16] : Set GPIO pin 16
+	  spin_sleep_ms(5000);
+	  *GPIO_CLR0 |= (1<<16); //GPIO_CLR0[16] : Clear GPIO pin 16
+	  spin_sleep_ms(5000);
+  }
 }
